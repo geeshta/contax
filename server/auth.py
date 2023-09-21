1440a6b8d504362b378e0b53ff435e55d5a52c7f
geeshta <stefan.foldesi@outlook.com>
2023-09-22 01:35:40 +0200

    Simplify user handler

--- server/auth.py
+++ server/auth.py
@@ -4,43 +4,27 @@ from litestar.middleware.session.client_side import ClientSideSessionBackend
 from litestar.security.session_auth import SessionAuth
 from litestar.status_codes import HTTP_401_UNAUTHORIZED
 from sqlalchemy.ext.asyncio import AsyncSession
-from typing import AsyncGenerator
-from server.logging import Logger
 from server.session import AppSession, SessionProxy, session_config
 from server.users.models import User
-from server.users.service import UserService
+from sqlalchemy import select
 
 
 async def retrieve_user_handler(
     session: AppSession, connection: ASGIConnection
 ) -> User | None:
     db_session_provider = connection.app.dependencies["db_session"]
-    logger_provider = connection.app.dependencies["logger"]
-    transaction_provider = connection.app.dependencies["transaction"]
-    user_service_provider = connection.app.dependencies["user_service"]
 
     db_session: AsyncSession = await db_session_provider(
         state=connection.app.state, scope=connection.scope
     )
-    logger: Logger = await logger_provider()
-    transaction_generator: AsyncGenerator[
-        AsyncSession, None
-    ] = await transaction_provider(db_session=db_session)
 
-    transaction = await anext(transaction_generator)
-
-    try:
-        user_service: UserService = await user_service_provider(
-            logger=logger, db_session=db_session, transaction=transaction_generator
-        )
-        if "user_id" not in session:
-            return None
-
-        user = await user_service.get_by_id(session["user_id"], raise_404=False)
+    if "user_id" in session:
+        query = select(User).where(User.id == session["user_id"])
+        result = await db_session.execute(query)
+        user = result.scalar_one_or_none()
         return user
 
-    finally:
-        await transaction.aclose()
+    return None
 
 
 session_auth = SessionAuth[User, ClientSideSessionBackend](
