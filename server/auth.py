2e049dc31b50bd72af7a268b245770579e0b24dd
geeshta <stefan.foldesi@outlook.com>
2023-09-21 02:48:40 +0200

    Moved helper function to file

--- server/auth.py
+++ server/auth.py
@@ -2,7 +2,7 @@ from litestar.middleware.session.client_side import ClientSideSessionBackend
 from litestar.security.session_auth import SessionAuth
 
 from server.session import session_config
-from server.users import retrieve_user_handler
+from server.users.helpers import retrieve_user_handler
 from server.users.models import User
 
 session_auth = SessionAuth[User, ClientSideSessionBackend](
