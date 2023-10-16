a247c1281835973b73bf9408087af706fc43ab12
geeshta <stefan.foldesi@outlook.com>
2023-10-16 22:08:04 +0200

    Update contact handler

--- server/contacts/controllers.py
+++ server/contacts/controllers.py
@@ -76,19 +76,29 @@ class ContactPageController(Controller):
     path = "/contacts"
     dependencies = {"contact_service": Provide(provide_contact_service)}
 
-    async def render_contact_page(
+    async def render_contact_list_page(
         self, form: ContactForm, contact_service: ContactService
-    ):
+    ) -> Template:
         contacts = await contact_service.get_user_contacts()
         return Template(
             "contacts/contact_list.html.j2",
             context={"contacts": contacts, "form": form},
         )
 
+    async def render_contact_detail_page(
+        self, form: ContactForm, contact: Contact
+    ) -> Template:
+        return Template(
+            "contacts/contact_detail.html.j2",
+            context={"contact": contact, "form": form},
+        )
+
     @get("/", name="contact_list_page")
     async def list_contacts(self, contact_service: ContactService) -> Template:
         form = ContactForm()
-        return await self.render_contact_page(form, contact_service)
+        return await self.render_contact_list_page(
+            form=form, contact_service=contact_service
+        )
 
     @post("/", name="process_contact_form")
     async def create_contact(
@@ -103,15 +113,17 @@ class ContactPageController(Controller):
             )
             contact_page_url = request.app.route_reverse("contact_list_page")
             return Redirect(contact_page_url, status_code=HTTP_303_SEE_OTHER)
-        return await self.render_contact_page(form, contact_service)
+        return await self.render_contact_list_page(form, contact_service)
 
-    @get("/{id:int}")
+    @get("/{id:int}", name="contact_detail_page")
     async def retrieve_contact(
         self, id: int, contact_service: ContactService
     ) -> Template:
-        ...
+        contact = await contact_service.get_user_contact_by_id(id)
+        form = ContactForm(data=contact.to_dict())
+        return await self.render_contact_detail_page(contact=contact, form=form)
 
-    @post("/{id:int}")
+    @post("/{id:int}", name="update_contact_form")
     async def update_contact(
         self,
         id: int,
@@ -119,8 +131,18 @@ class ContactPageController(Controller):
         contact_service: ContactService,
         request: Request,
     ) -> Template | Redirect:
-        ...
+        form = ContactForm(data=data)
+        if form.validate():
+            await contact_service.update_contact(
+                id=id,
+                name=form.name.data,  # type: ignore
+                phone_number=form.phone_number.data,
+                email=form.email.data,
+            )
+            return Redirect(request.app.route_reverse("contact_list_page"))
+        contact = await contact_service.get_user_contact_by_id(id)
+        return await self.render_contact_detail_page(contact=contact, form=form)
 
-    @post("/{id:int}/delete")
+    @post("/{id:int}/delete", name="delete_contact")
     async def delete_contact(self, id: int, request: Request) -> Redirect:
         ...
